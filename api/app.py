76e6fa4564682909781528d782d83acab04d7b05
bodelha <bodelha@gmail.com>
2024-10-20 21:44:32 -0300

    Hello world api

---
+++ api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from schema import HelloWorldSchema
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    response_data = {"message": "Hello, World!"}
+    
+    schema = HelloWorldSchema()
+    result = schema.dump(response_data)
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
