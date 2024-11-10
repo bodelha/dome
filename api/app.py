82bc99d1ede224e400239baef89978797b793cd7
bodelha <bodelha@gmail.com>
2024-11-09 21:44:00 -0300

    Implementação do GET paginado com query params

--- api/app.py
+++ api/app.py
@@ -16,7 +16,6 @@ def receive_measurements():
         app.logger.info(data)
         db.connect()
         for field in data:
-            app.logger.info(field)
             if field in ("time", "batteryLevel"):
                 continue
             get_table_query = "SELECT id, sensor_type FROM sensors_catalog WHERE sensor_name = %s AND status = 'active';"
@@ -35,6 +34,65 @@ def receive_measurements():
         app.logger.exception(f"An error occurred while persisting data: {e}")
         return jsonify({"message": "Erro ao salvar os dados."}), 500
 
+@app.route('/v1/measurements', methods=['GET'])
+def get_measurements():
+    limit = request.args.get('limit', default=10, type=int)
+    offset = request.args.get('offset', default=0, type=int)
+
+    try:
+        db.connect()
+
+        query = """
+            SELECT
+                COALESCE(h.event_time_unix, e_h.event_time_unix, t.event_time_unix, e_t.event_time_unix, l.event_time_unix) AS time,
+                MAX(h.humidity) AS internalHumidity,
+                MAX(e_h.humidity) AS externalHumidity,
+                MAX(t.temperature) AS internalTemperature,
+                MAX(e_t.temperature) AS externalTemperature,
+                MAX(l.luminosity) AS luminosity
+            FROM
+                sensors_catalog sc
+            LEFT JOIN humidity h ON sc.id = h.sensor_id AND sc.sensor_name = 'internalHumidity'
+            LEFT JOIN humidity e_h ON sc.id = e_h.sensor_id AND sc.sensor_name = 'externalHumidity'
+            LEFT JOIN temperature t ON sc.id = t.sensor_id AND sc.sensor_name = 'internalTemperature'
+            LEFT JOIN temperature e_t ON sc.id = e_t.sensor_id AND sc.sensor_name = 'externalTemperature'
+            LEFT JOIN luminosity l ON sc.id = l.sensor_id AND sc.sensor_name = 'luminosity'
+            WHERE sc.status = 'active'
+            GROUP BY time
+            ORDER BY time DESC
+            LIMIT %s OFFSET %s;
+        """
+
+        measurements = db.execute(query, (limit, offset)).fetchall()
+
+        app.logger.info(measurements)
+
+        formatted_measurements = []
+        for entry in measurements:
+            formatted_measurement = {
+                "time": entry[0],
+                "internalHumidity": entry[1],
+                "externalHumidity": entry[2],
+                "internalTemperature": entry[3],
+                "externalTemperature": entry[4],
+                "luminosity": entry[5]
+            }
+            formatted_measurements.append(formatted_measurement)
+
+        app.logger.info(formatted_measurements)
+
+        schema = SensorMeasurementV1Schema(many=True)
+        result = schema.dump(formatted_measurements)
+        app.logger.info(result)
+
+        db.close()
+
+        return jsonify({"data": result, "limit": limit, "offset": offset}), 200
+
+    except Exception as e:
+        app.logger.exception(f"An error occurred while fetching data: {e}")
+        return jsonify({"message": "Erro ao recuperar os dados."}), 500
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
@@ -44,6 +102,7 @@ def hello_world():
     result = schema.dump(response_data)
 
     return jsonify(result)
+
 
 if __name__ == '__main__':
 
