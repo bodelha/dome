4c83cebe4e25ed95a4d6f3852aa203905515bcb3
bodelha <bodelha@gmail.com>
2024-11-17 19:52:16 -0300

    Singleton para gerenciar conexão no banco

---
+++ api/controllers/v1/sensor_data.py
@@ -0,0 +1,88 @@
+from flask import request, jsonify
+from db import Database
+
+
+def get_sensor_data():
+    db = Database()
+    fields = request.args.get("fields", None)
+    limit = request.args.get("limit", 60, type=int)
+    fields = (
+        fields.split(",")
+        if fields
+        else [
+            "internalHumidity",
+            "externalHumidity",
+            "internalTemperature",
+            "externalTemperature",
+            "luminosity",
+        ]
+    )
+
+    try:
+        db.connect()
+
+        field_mapping = {
+            "internalHumidity": "MAX(CASE WHEN sensor_name = 'internalHumidity' THEN value END) AS \"internalHumidity\"",
+            "externalHumidity": "MAX(CASE WHEN sensor_name = 'externalHumidity' THEN value END) AS \"externalHumidity\"",
+            "internalTemperature": "MAX(CASE WHEN sensor_name = 'internalTemperature' THEN value END) AS \"internalTemperature\"",
+            "externalTemperature": "MAX(CASE WHEN sensor_name = 'externalTemperature' THEN value END) AS \"externalTemperature\"",
+            "luminosity": "MAX(CASE WHEN sensor_name = 'luminosity' THEN value END) AS \"luminosity\""
+        }
+
+        selected_fields = ", ".join([field_mapping[field] for field in fields if field in field_mapping])
+
+        json_fields = ", ".join([f"'{field}', aggregated_data.\"{field}\"" for field in fields])
+
+        query = f"""
+            WITH sensor_data AS (
+                SELECT 
+                    s.sensor_name,
+                    s.sensor_type,
+                    CASE 
+                        WHEN s.sensor_type = 'temperature' THEN t.event_time_unix
+                        WHEN s.sensor_type = 'humidity' THEN h.event_time_unix
+                        WHEN s.sensor_type = 'luminosity' THEN l.event_time_unix
+                    END AS event_time_unix,
+                    CASE 
+                        WHEN s.sensor_type = 'temperature' THEN t.temperature
+                        WHEN s.sensor_type = 'humidity' THEN h.humidity
+                        WHEN s.sensor_type = 'luminosity' THEN l.luminosity
+                    END AS value
+                FROM 
+                    sensors_catalog s
+                LEFT JOIN temperature t ON s.id = t.sensor_id
+                LEFT JOIN humidity h ON s.id = h.sensor_id
+                LEFT JOIN luminosity l ON s.id = l.sensor_id
+                WHERE 
+                    s.status = 'active'
+            ),
+            aggregated_data AS (
+                SELECT 
+                    event_time_unix AS time,
+                    {selected_fields}
+                FROM 
+                    sensor_data
+                GROUP BY event_time_unix
+                ORDER BY event_time_unix DESC
+                LIMIT {limit}
+            )
+            SELECT jsonb_agg(jsonb_strip_nulls(jsonb_build_object({json_fields}))) AS sensor_data, array_agg(time) AS time
+            FROM aggregated_data;
+        """
+
+        result = db.execute(query).fetchone()
+
+        data = result[0] if result else []
+        time_list = result[1] if result else []
+
+        formatted_data = {"time": time_list}
+        for field in fields:
+            formatted_data[field] = [entry.get(field) for entry in data]
+
+        return jsonify(formatted_data)
+
+    except Exception as e:
+        db.close()
+        raise e
+    finally:
+        db.close()
--- api/db.py
+++ api/db.py
@@ -5,39 +5,50 @@ from utils import logger
 
 
 class Database:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Database, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self.database_url = os.getenv("DATABASE_URL")
-        self.conn = None
+        if not hasattr(self, "initialized"):
+            self.database_url = os.getenv("DATABASE_URL")
+            self.conn = None
+            self.initialized = True
 
     def connect(self):
-        if not self.conn:
+        if not self.conn or self.conn.closed:
             try:
                 self.conn = psycopg2.connect(self.database_url)
-                logger.info("Connected to PostgreSQL database!")
+                logger.debug("Connected to PostgreSQL database!")
             except (Exception, psycopg2.Error) as error:
                 logger.error("Error connecting to PostgreSQL database:", error)
                 raise
 
     def execute(self, query, params=None):
-        if not self.conn:
+        if not self.conn or self.conn.closed:
             self.connect()
-        with self.conn.cursor() as cursor:
-            logger.debug("Executing query: %s with: %s", query, params)
-            cursor.execute(query, params)
-            return cursor
+        cursor = self.conn.cursor()
+        logger.debug("Executing query: %s with: %s", query, params)
+        cursor.execute(query, params)
+        return cursor
 
     def fetchall(self, query, params=None):
-        logger.debug("Fetchall: %s with: %s", query, params)
-        with self.execute(query, params) as cursor:
-            return cursor.fetchall()
+        cursor = self.execute(query, params)
+        results = cursor.fetchall()
+        cursor.close()
+        return results
 
     def fetchone(self, query, params=None):
-        logger.debug("Fetchone: %s with: %s", query, params)
-        with self.execute(query, params) as cursor:
-            return cursor.fetchone()
+        cursor = self.execute(query, params)
+        result = cursor.fetchone()
+        cursor.close()
+        return result
 
     def close(self):
         if self.conn:
             self.conn.close()
             self.conn = None
-            logger.info("Connection to PostgreSQL database closed.")
+            logger.debug("Connection to PostgreSQL database closed.")
