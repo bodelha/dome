f5bf8652ab8ad05bd848bc4c1b826ffc3fc41fba
Kauê José <jkaue986@gmail.com>
2024-11-20 15:27:07 -0300

    Mobile Final

--- mobile/mApp/app/dados.tsx
+++ mobile/mApp/app/dados.tsx
@@ -1,33 +1,47 @@
-import {
-  StyleSheet,
-  Text,
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function TabTwoScreen() {
   return (
-    <>
-    <Text style={styles.iconText}>Dome</Text>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.iconText}>Dome</Text>
+      <WebView
+        style={styles.webview}
+        source={{
+          uri: "https://app.powerbi.com/view?r=eyJrIjoiMTE5ZTU4NDYtOTc0OS00MDQyLTkwMzctMGYxZGVmODU2YTFmIiwidCI6ImNmNzJlMmJkLTdhMmItNDc4My1iZGViLTM5ZDU3YjA3Zjc2ZiIsImMiOjR9",
+        }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
   iconText: {
     position: 'absolute',
-    top: 30,
+    top: 20,
     left: 10,
     fontSize: 24,
     fontWeight: 'bold',
     color: '#02A676',
   },
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  webview: {
+    marginTop: 80,
+    flex: 1,
+    height: '100%',
+    width: 400,
+    borderWidth: 2,
+    borderColor: '#D6D58E',
+    borderRadius: 10,
   },
 });
